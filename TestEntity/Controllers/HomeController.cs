e4322b926e4f1245157dd4517d67339a8a73b5f3
BrettMerri <the.b.merri@gmail.com>
2017-05-13 00:21:08 -0400

    initial commit

---
+++ TestEntity/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestEntity.Models;
+
+namespace TestEntity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            NorthwindEntities NorthwindDB = new NorthwindEntities();
+
+            int amountOfCustomers = NorthwindDB.Customers.Count();
+
+            ViewBag.Message = $"We have {amountOfCustomers} Customers";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestEntity/Controllers/NorthwindController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestEntity.Models;
+
+namespace TestEntity.Controllers
+{
+    public class NorthwindController : Controller
+    {
+        // GET: Northwind
+        public ActionResult ListAllCustomers()
+        {
+            List<Customer> CustomerList = GetCustomerList();
+            List<string> CityList = GetCityList();
+
+            ViewBag.SearchResultsList = CustomerList;
+            ViewBag.CityList = CityList;
+
+            return View();
+        }
+
+        public ActionResult SearchCustomersByCustomerID(string CustomerID)
+        {
+            if (CustomerID == null || CustomerID == "")
+            {
+                return RedirectToAction("ListAllCustomers");
+            }
+
+            List<Customer> CustomerList = GetCustomerList();
+            List<string> CityList = GetCityList();
+
+            List<Customer> SearchResultsList = CustomerList
+                .Where(x => x.CustomerID.ToUpper()
+                .Contains(CustomerID.ToUpper()))
+                .ToList();
+
+            ViewBag.SearchResultsList = SearchResultsList;
+            ViewBag.CityList = CityList;
+            ViewBag.CustomerIDSelection = CustomerID;
+
+            return View("ListAllCustomers");
+        }
+
+        public ActionResult SearchCustomersByCity(string City)
+        {
+            List<Customer> CustomerList = GetCustomerList();
+            List<string> CityList = GetCityList();
+
+            if (City == "Show all")
+            {
+                return RedirectToAction("ListAllCustomers");
+            }
+
+            List<Customer> SearchResultsList = CustomerList
+                .Where(x => x.City !=null && x.City
+                .Equals(City))
+                .ToList();
+
+            ViewBag.SearchResultsList = SearchResultsList;
+            ViewBag.CityList = CityList;
+            ViewBag.CitySelection = City;
+
+            return View("ListAllCustomers");
+        }
+
+        public List<Customer> GetCustomerList()
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+            return NE.Customers.ToList();
+        }
+
+        public List<string> GetCityList()
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+            return NE.Customers.Where(x => x.City != null)
+                .Select(x => x.City)
+                .Distinct()
+                .ToList();
+        }
+    }
+}
